dd3779551f0a897ddf56ae484d1dfdca49aec319
Yehns <118644844+Yehns@users.noreply.github.com>
2023-04-05 13:45:36 +0800

    Update bushfire_logic.js

--- bushfire_logic.js
+++ bushfire_logic.js
@@ -10,51 +10,66 @@ d3.json(bushfires_url).then(function(data) {
 function createFeatures(bushfireData) {
 
   // Define a function that we want to run once for each feature in the features array.
-  // Give each feature a popup that describes the place and time of the bushfire.
+  // Give each feature a popup that describes the metadata of each bushfire.
   function onEachFeature(feature, layer) {
-    layer.bindPopup(`<h2>${feature.properties.fih_year1}</h3><hr><p>${(feature.properties.fih_poly_type)}</p><hr><hs><h2>${feature.properties.fih_comment}</h3></hr><hr><p>`);
+    layer.bindPopup(`<h2>${feature.properties.fih_hist_distr}</h3><hr><p>Fire Location: ${(feature.properties.fih_name)}</p></h3><p>Burn Type: ${(feature.properties.fih_poly_type)}</p></h3><p> DFES Report:${(feature.properties.fih_comment)}<p>${(feature.properties.fih_year1)}</p></h3><p></p><hr><hs><h2>${feature.properties.fih_hectares.toFixed(0)} Hectares</h3></hr><p>`);
   }
 
   // Create a GeoJSON layer that contains the features array on the bushfires object.
   // Run the onEachFeature function once for each piece of data in the array.
+  // Define the polygon colours
   let bushfires = L.geoJSON(bushfireData, {
-    onEachFeature: onEachFeature
+    onEachFeature: onEachFeature,
+    color: "red",
+    fillColor: "red",
+    stroke: 0.5
   });
-
-  // Send our bushfires layer to the createMap function
-  createMap(bushfires);
-}
 
 // Store our API endpoint as queryUrl.
 let firestations_url = "http://127.0.0.1:5500/Project%203/fire_station_districts.geojson";
 
-// Perform a GET request to the query URL/
-d3.json(firestations_url).then(function(data1) {
+// Perform a GET request to the query URL
+d3.json(firestations_url).then(function(data) {
   // Once we get a response, send the data.features object to the createFeatures function.
-  createFeatures(data1.features);
+  createFeatures(data.features);
 });
 
 function createFeatures(firestationData) {
 
   // Define a function that we want to run once for each feature in the features array.
-  // Give each feature a popup that describes the place and district of the DFES zones
   function onEachFeature(feature, layer) {
-    layer.bindPopup(`<h2>${feature.properties.fih_year1}</h3><hr><p>${(feature.properties.fih_poly_type)}</p><hr><hs><h2>${feature.properties.fih_comment}</h3></hr><hr><p>`);
+    layer.on({
+      // When a user's mouse cursor touches a map feature, the mouseover event calls this function, which makes that feature's opacity change to 40% so that it stands out.
+      mouseover: function(event) {
+        layer = event.target;
+        layer.setStyle({
+          fillOpacity: 0.4
+        });
+      },
+      // When the cursor no longer hovers over a map feature (that is, when the mouseout event occurs), the feature's opacity reverts back to 20%.
+      mouseout: function(event) {
+        layer = event.target;
+        layer.setStyle({
+          fillOpacity: 0.2
+        });
+      },
+
+    });
+
+     // Give each feature a popup that describes the metadata for DFES zones
+    layer.bindPopup(`<h2>${feature.properties.name}</h3><hr><p>Zone Type: ${(feature.properties.dist_type)}</p></h3><p>Hazard Management Agency: ${(feature.properties.hma)}</p><h3><p>Perimeter Size: ${(feature.properties.st_perimeter_shape_.toFixed(0))} meters</h3></hr><hr></p>`);
   }
 
   // Create a GeoJSON layer that contains the features array on the firestations object.
   // Run the onEachFeature function once for each piece of data in the array.
   let firestations = L.geoJSON(firestationData, {
-    onEachFeature: onEachFeature
+    onEachFeature: onEachFeature,
+    color: "blue",
+    fillColor: "blue",
   });
 
-  // Send our firestations layer to the createMap function/
-  createMap(firestations);
-}
-
-function createMap(allData) {
-
   // Create the base layers.
+
   let street = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
   })
@@ -71,7 +86,8 @@ function createMap(allData) {
 
   // Create an overlay object to hold our overlay.
   let overlayMaps = {
-    "Last 100 Bushfires" : allData
+    "Last 100 Bushfires": bushfires,
+    "DFES Zones": firestations
   };
 
   // Create our map, giving it the streetmap and bushfires and firestations layers to display on load.
@@ -80,18 +96,15 @@ function createMap(allData) {
       -32, 116
     ],
     zoom: 8,
-    layers: [street, allData]
+    layers: [street, bushfires, firestations]
   });
+  
 
   // Create a layer control.
   // Pass it our baseMaps and overlayMaps.
   // Add the layer control to the map.
   L.control.layers(baseMaps, overlayMaps, {
     collapsed: false
-  }).addTo(myMap);
+  }).addTo(myMap)
+}}; 
 
-}
-
-
-// let bushfires_link = "http://127.0.0.1:5500/Project%203/Top_100_Bushfires.geojson";
-// let firestations_link = "http://127.0.0.1:5500/Project%203/fire_station_districts.geojson"
