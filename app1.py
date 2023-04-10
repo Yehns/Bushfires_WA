1f72a882c9968d231c7a17d3900b23ad2a8315f2
Paul-Roberts-17 <merovingi@hotmail.com>
2023-04-10 20:31:21 +0800

    tidied file

--- app1.py
+++ app1.py
@@ -29,18 +29,29 @@ def welcome():
     """List all available api routes."""
     return (
         f"Available Routes:<br/>"
-        f"/api/v1.0/coords<br/>"
+        f"/api/v1.0/coords2020<br/>"
+        f"/api/v1.0/coords2021<br/>"
         f"/api/v1.0/per_year<br/>"
         f"/api/v1.0/season<br/>"
         f"/api/v1.0/"
     )
 
-
-@app.route("/api/v1.0/coords")
-def coord():
+@app.route("/api/v1.0/coords2021")
+def c2021():
     #2) get a coord data
     session = Session(engine)
-    results = session.query(coords.coords, coords.year, coords.date, coords.season, coords.district).filter(coords.year >= 1970).all()
+    results = session.query(coords.coords, coords.year, coords.date, coords.season, coords.district).filter(coords.year == 2021).all()
+    session.close()
+
+    #all_names = list(np.ravel(results))
+
+    return jsonify([[v,w,x,y,z] for v,w,x,y,z in results])
+
+@app.route("/api/v1.0/coords2020")
+def c2020():
+    #2) get a coord data
+    session = Session(engine)
+    results = session.query(coords.coords, coords.year, coords.date, coords.season, coords.district).filter(coords.year == 2020).all()
     session.close()
 
     #all_names = list(np.ravel(results))
@@ -53,7 +64,6 @@ def year():
     session = Session(engine)
     results = session.query(per_year.year, per_year.count).all()
     session.close()
-    print(results)
     #all_names = list(np.ravel(results))
     #print(all_names)
     return jsonify([[int(x),int(y)] for x,y in results])
@@ -68,43 +78,6 @@ def seas():
     #all_names = list(np.ravel(results))
 
     return jsonify([[x,y,z] for x,y,z in results])
-
-# @app.route("/api/v1.0/tobs")
-# def tobs_yr():
-#     #4) tobs for USC00519281 for last year
-#     start_point = dt.date(2017, 8, 23) - dt.timedelta(days=365)
-
-#     session = Session(engine)
-#     tobs_yr = session.query(measurement.tobs).filter(measurement.station == 'USC00519281').filter(measurement.date >= start_point).all()
-#     session.close()
-
-#     results = list(np.ravel(tobs_yr))
-
-#     return jsonify(results)
-
-# @app.route("/api/v1.0/<start>")
-# def start_date(start):
-#     #5) return the min, avg and max temperature from a variable start date to the end of data
-#     session = Session(engine)
-#     start_query = session.query(func.min(measurement.tobs), func.avg(measurement.tobs), func.max(measurement.tobs)).filter(measurement.date >= start).all()
-#     session.close()
-
-#     results = list(np.ravel(start_query))
-
-#     return jsonify(results)
-
-# @app.route("/api/v1.0/<start>/<end>")
-# def start_end_date(start,end):
-#     #5) return the min, avg and max temperature from a variable start date to a variable end date
-#     session = Session(engine)
-#     start_query = session.query(func.min(measurement.tobs), func.avg(measurement.tobs), func.max(measurement.tobs)).filter(measurement.date >= start).filter(measurement.date <= end).all()
-#     session.close()
-
-#     results = list(np.ravel(start_query))
-
-#     return jsonify(results)
-
-
 
 
 if __name__ == '__main__':
